1975d212add78e5b5861cfa4995ab4ecda5c2221
Dante1999033 <wlad.scheludko@gmail.com>
2020-09-08 11:40:39 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux'
+import rootReducer from './reducers'
+//import thunk from 'redux-thunk';
+//import * as reducers from './store/reducers';
+// import './index.css';
+//import App from './App';
+import App from './components/App'
 import * as serviceWorker from './serviceWorker';
 
+
+const store = createStore(rootReducer,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+      {/* <React.StrictMode> */}
+        <App />
+      {/* </React.StrictMode> */}
+  </Provider>
+  ,
   document.getElementById('root')
 );
 
